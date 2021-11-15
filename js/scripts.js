60ad227c4ab451082db3cbacbb4d51146204b328
pretala8 <myrickmichelle314@gmail.com>
2021-11-15 13:30:28 -0500

    Create menuToggle function for menu icon

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
-console.log("Hi, welcome to my portfolio site! :)")
+function menuToggle(){
+  var x = document.getElementById("myNavtoggle");//Searches doc for elem with ID provided
+  //Hide menu
+  if (x.className === "navtoggle"){
+    x.className += " responsive";//Adds classname 'responsive' to classname of x
+  }else {
+    x.className = "navtoggle";
+  }
+}
